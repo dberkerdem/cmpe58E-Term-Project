a8f7deabed7539547fb890e4a7c90269edad3217
dberkerdem <dberkerdem@gmail.com>
2023-12-25 03:22:32 +0300

    created GKE deployment

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+import socket
 
+hostname = socket.gethostname()
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World/v1"}
+    return {"message": f"Hello World from {hostname}. (v1)"}
